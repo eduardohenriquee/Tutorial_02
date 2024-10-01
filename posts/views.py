164adbea2095f19510af10b8995c173141ef891b
Eduardo-Felisbino <eduardo.felisbino@bialog.com>
2024-10-01 10:37:31 -0300

    tutorial

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,5 @@ from django.views.generic import ListView
 from .models import Post 
 
 class HomePageView(ListView):
-   model = Post 
-   template_name = 'home.html'
-   context_object_name = 'all_posts_list'
+	model = Post 
+	template_name = 'home.html'
--- website/urls.py
+++ website/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-# views importadas
-from hello_world import views
+from django.urls import path, include
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	# URL configurada
-	path('', views.index, name="homepage")
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')),
 ]
